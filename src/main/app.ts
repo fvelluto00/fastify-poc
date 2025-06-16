4c37efcdfb310dffa8b2b7cc38ba87de8c1d0ae9
Francesco Velluto <f.velluto@reply.it>
2025-06-16 11:47:43 +0200

    Server boot fixes and docker compose support added

--- src/main/app.ts
+++ src/main/app.ts
@@ -6,7 +6,7 @@ const startServer = async() => {
     const fastify: FastifyInstance = buildServer({ logger: true });
 
     try {
-        const address: string = await fastify.listen({ port: PORT });
+        const address: string = await fastify.listen({ port: PORT, host: '0.0.0.0' });
         fastify.log.info(`Server listening at ${address}:${PORT}`);
     } catch(error) {
         fastify.log.error(error, "Error while starting server");
